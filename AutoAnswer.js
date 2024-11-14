22430ccd10bf1d3e5c768bf6602b2d8cb77b855d
Gladimore <105724638+Gladimore@users.noreply.github.com>
2024-11-14 09:40:27 -0600

    Update AutoAnswer.js

--- AutoAnswer.js
+++ AutoAnswer.js
@@ -248,7 +248,7 @@ class QuizHandler {
   }
 
   async send() {
-    const prompt = `These are the options: ${this.getAllOptions()}. To this problem: ${this.question}`
+    const prompt = `These are the options: ${this.getAllOptions()}. To this problem: ${this.question}. I only need the answer, nothing else. For example option 1.`
 
     try {
       const response = await fetch(
